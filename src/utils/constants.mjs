9960b4109328803ed435ecd0584b209007880bd0
maurizio.schifano <>
2024-06-24 18:40:28 +0200

    Bottone navigazione pagine

---
+++ src/utils/constants.mjs
@@ -0,0 +1,54 @@
+/** @format */
+
+export const pageNames = {
+  page01: {
+    title: "GLI ATTREZZI DELLA SALINA",
+    sectionNumber: "01",
+    sectionNumber: "01",
+    image: "https://images.unsplash.com/photo-1440579262733-eb456b312d26?q=80&w=4878&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "",
+  },
+  page02: {
+    title: "Setaccio",
+    sectionNumber: "02",
+    image: "https://images.unsplash.com/photo-1565746461434-a5100a1d812b?q=80&w=5670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "",
+  },
+  page03: {
+    title: "Spira di archimede",
+    sectionNumber: "03",
+    image: "https://images.unsplash.com/photo-1561472114-64f81c541347?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "",
+  },
+  page04: {
+    title: "Rastreddru",
+    sectionNumber: "04",
+    image: "https://images.unsplash.com/photo-1631266524189-b2c304f1fa83?q=80&w=4000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "",
+  },
+  page05: {
+    title: "Pala di rumpiri",
+    sectionNumber: "05",
+    image: "https://images.unsplash.com/photo-1636747242721-57375c7c0f22?q=80&w=2075&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "",
+  },
+  page06: {
+    title: "Pala di carricatu",
+    sectionNumber: "06",
+    image: "https://images.unsplash.com/photo-1631266524189-b2c304f1fa83?q=80&w=4000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "",
+  },
+  page07: {
+    title: "Paluneddu",
+    sectionNumber: "07",
+    image: "https://images.unsplash.com/photo-1541345544687-ef6c84c669cd?q=80&w=4985&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "",
+  },
+  page08: {
+    title: "Carriola",
+    sectionNumber: "08",
+    image: "https://images.unsplash.com/photo-1561472114-64f81c541347?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "",
+  },
+};
+

064c537e2f70fd78601f5a50f4d1e978bf83985b
maurizio.schifano <>
2024-06-24 07:56:09 +0200

    Setup iniziale

---
+++ src/style/theme.mjs
@@ -0,0 +1,39 @@
+/** @format */
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+        headings: ["Montserrat", "sans-serif"],
+      },
+
+      colors: {
+        primary: {
+          DEFAULT: "#e77968",
+          100: "#fdf5f4",
+          200: "#fae8e6",
+          300: "#f5cfca",
+          400: "#efb1aa",
+          500: "#e77968",
+          600: "#da7262",
+          700: "#bd6355",
+          800: "#9a5145",
+          900: "#6d3931",
+        },
+        neutral: {
+          DEFAULT: "#999999",
+          100: "#f2f2f2",
+          200: "#e1e1e1",
+          300: "#bfbfbf",
+          400: "#969595",
+          500: "#1f1c1c",
+          600: "#1d1a1a",
+          700: "#191717",
+          800: "#151313",
+          900: "#0f0d0d",
+        },
+      },
+    },
+  },
+};

f150d8bb045bccfb176d0e8fff9342869bb96395
Maurizio <maurizio.schifano@runelab.it>
2024-07-06 00:52:58 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,11 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  site: 'https://machiebruce.github.io',
+  site: "https://machiebruce.github.io",
+  build: {
+    // Example: Generate `page.html` instead of `page/index.html` during build.
+    format: "preserve",
+  },
 });
 
 

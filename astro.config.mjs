55685436406558d052dc4c44e7d1d37f65c0284d
Maurizio <maurizio.schifano@runelab.it>
2024-07-06 01:11:54 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ export default defineConfig({
   integrations: [tailwind(), mdx()],
   site: "https://machiebruce.github.io",
   base: "/sei-museum",
+  output: 'static',
 
   build: {
     // Example: Generate `page.html` instead of `page/index.html` during build.

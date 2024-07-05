3a193374877ff5396cc0b817dbf5f735223c1746
Maurizio <maurizio.schifano@runelab.it>
2024-07-06 00:30:09 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
 
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   site: 'https://machiebruce.github.io',
-  base: '/sei-museum',
 });
 
 

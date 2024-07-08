3904593e7ab9613a890a87ac8a39c41837fd8241
maurizio.schifano <>
2024-07-08 12:36:28 +0200

    riportate fix del totem locale

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,14 @@ import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
 
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   site: "https://machiebruce.github.io",
   base: "/sei-museum",
   output: "static",
-
   build: {
     // Example: Generate `page.html` instead of `page/index.html` during build.
-    format: "preserve",
-  },
+    format: "preserve"
+  }
 });
-
-
